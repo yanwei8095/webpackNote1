ed60556fae652fabdb64562039738ad35cecddbc
yanwei8095 <yanwei2007095@qq.com>
2020-06-09 15:24:08 +0800

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import add from "./module1";
+
+import {sum,mul} from "./module2";
+
+import {name,age} from "./module3";
+import data from "../json/data";
+
+import "../less/01.less";
+import "../less/02.less"
+
+console.log(add(3,4));
+console.log(sum(1,2,3,4));
+console.log(mul(2,3));
+console.log(name,age);
+console.log(data);
---
+++ src/js/module3.js
@@ -0,0 +1,4 @@
+// 分别暴露
+export const name="yanwei";
+
+export const age = 30;
